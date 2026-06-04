daa5a4a71707853ff7a054cd586b2ef3488bc241
karthikristen-ux <karthikristen-ux@users.noreply.github.com>
2026-06-04 22:02:32 +0530

    Remove intro loading screen - go directly to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
 import { Home } from './pages/Home';
 import { Projects } from './pages/Projects';
 import { Certificates } from './pages/Certificates';
 import { Hobbies } from './pages/Hobbies';
 import { MissMinutes } from './components/MissMinutes';
-import { TVALoadingScreen } from './components/TVALoadingScreen';
 import { Clock } from 'lucide-react';
 
 const Navigation = () => {
@@ -29,47 +27,19 @@ const Navigation = () => {
 };
 
 const App: React.FC = () => {
-  const [showIntro, setShowIntro] = useState(true);
-
-  useEffect(() => {
-    // Check if intro has already been played this session
-    if (sessionStorage.getItem('tva-intro-played') === 'true') {
-      setShowIntro(false);
-    }
-  }, []);
-
-  const handleIntroComplete = () => {
-    sessionStorage.setItem('tva-intro-played', 'true');
-    setShowIntro(false);
-  };
-
   return (
-    <>
-      <AnimatePresence>
-        {showIntro && (
-          <motion.div 
-            key="tva-intro" 
-            exit={{ opacity: 0 }} 
-            transition={{ duration: 1 }}
-            style={{ position: 'fixed', zIndex: 99999, top: 0, left: 0, width: '100%', height: '100%' }}
-          >
-            <TVALoadingScreen onComplete={handleIntroComplete} />
-          </motion.div>
-        )}
-      </AnimatePresence>
-
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/certificates" element={<Certificates />} />
-          <Route path="/hobbies" element={<Hobbies />} />
-        </Routes>
-        <MissMinutes />
-      </Router>
-    </>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/certificates" element={<Certificates />} />
+        <Route path="/hobbies" element={<Hobbies />} />
+      </Routes>
+      <MissMinutes />
+    </Router>
   );
 };
 
 export default App;
+
