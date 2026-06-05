f0f97e0d60f1725136b1933ef96079abaa003ad9
karthikristen-ux <karthikristen-ux@users.noreply.github.com>
2026-06-06 03:56:42 +0530

    Fix KRISTEN ARCHIVES logo color reverting to gray

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const Navigation = () => {
   return (
     <>
       <nav className={location.pathname !== '/' ? 'nav-collapsed' : ''}>
-        <Link to="/" className="logo" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <Link to="/" className="logo" style={{ textDecoration: 'none' }}>
           <Clock size={32} color="var(--tva-orange)" />
           KRISTEN ARCHIVES
         </Link>
